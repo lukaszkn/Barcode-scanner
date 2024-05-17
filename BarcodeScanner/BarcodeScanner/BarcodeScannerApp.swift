a2b4a7c5b98c7fc63d98c9761d40d564b428915a
Lukasz Knap <lukaszkn@go2.pl>
2024-05-17 18:05:21 +0200

    Project init

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Lukasz on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
